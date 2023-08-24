97e154504351150bbd44b909c3371ea13e2cb388
simone <simonegauli@gmail.com>
2023-08-24 20:45:47 +0200

    remove download function, use coraline.download instead

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import fs from 'node:fs';
-import https from 'node:https';
 import { PexelsImageOptions, PexelsVideoOptions, PhotosWithTotalResults, Videos } from './types';
 
 const pexelsapis = (apiKey: string) => {
@@ -38,27 +36,6 @@ const pexelsapis = (apiKey: string) => {
       const data: Videos = await res.json();
       if (!res.ok || !data.videos) throw new Error(`Pexels: ${data.status} ${data.error}`);
       return data.videos;
-    },
-    downloadVideo: (url: string, output: string) => {
-      return new Promise<string>((resolve, reject) => {
-        const downloadVideo = (url: string) => {
-          https.get(url, (res) => {
-            if (res.statusCode === 302) {
-              url = res.headers.location as string;
-              downloadVideo(url);
-              return;
-            } else if (res.statusCode !== 200) {
-              res.resume();
-              return reject(`Something went wrong when downloading a video from Pexels. ${res.statusCode} ${res.statusMessage}`);
-            }
-            res.pipe(fs.createWriteStream(output));
-            res.on('close', () => {
-              resolve(output);
-            });
-          });
-        };
-        downloadVideo(url);
-      });
     },
   };
 };
