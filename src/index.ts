65d18bc77e2bc804206ebef177c5b8d1de8f0868
simone <simonegauli@gmail.com>
2023-08-29 03:26:23 +0200

    switch to esmodule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,43 @@
-import { connect } from './lib';
+import { PexelsImageOptions, PexelsVideoOptions, PhotosWithTotalResults, Videos } from './types';
 
-const pexelsapis = {
-  connect,
+const pexelsapis = (apiKey: string) => {
+  const headers = {
+    Authorization: apiKey,
+  };
+  return {
+    getImage: async (text: string, options?: PexelsImageOptions) => {
+      let url = `https://api.pexels.com/v1/search?query=${text}`;
+      if (options) {
+        const usedOptions = Object.entries(options).filter(([, value]) => value !== undefined);
+        for (const [key, value] of usedOptions) {
+          url += `&${key}=${value}`;
+        }
+      }
+      const res = await fetch(url, {
+        method: 'GET',
+        headers,
+      });
+      const data: PhotosWithTotalResults = await res.json();
+      if (!res.ok) throw new Error('Pexels API error.');
+      return data.photos;
+    },
+    getVideo: async (text: string, options?: PexelsVideoOptions) => {
+      let url = `https://api.pexels.com/videos/search?query=${text}`;
+      if (options) {
+        const usedOptions = Object.entries(options).filter(([, value]) => value !== undefined);
+        for (const [key, value] of usedOptions) {
+          url += `&${key}=${value}`;
+        }
+      }
+      const res = await fetch(url, {
+        method: 'GET',
+        headers,
+      });
+      const data: Videos = await res.json();
+      if (!res.ok || !data.videos) throw new Error(`Pexels: ${data.status} ${data.error}`);
+      return data.videos;
+    },
+  };
 };
-
-export { connect } from './lib';
 
 export default pexelsapis;
