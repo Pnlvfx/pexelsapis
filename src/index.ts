acf09f1f69e2fca1179a37e4f0ca7a2063522add
simone <simonegauli@gmail.com>
2023-12-20 15:38:59 +0100

    fix unhandled error on fetch and bump deps

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { PexelsImageOptions, PexelsVideoOptions, PhotosWithTotalResults, Videos } from './types/index.js';
+import type { PexelsImageOptions, PexelsVideoOptions, PhotosWithTotalResults, Videos } from './types/index.js';
+
+const isJson = (res: Response) => res.headers.get('Content-Type')?.includes('application/json');
 
 const pexelsapis = (apiKey: string) => {
   const headers = {
     Authorization: apiKey,
   };
   return {
-    getImage: async (text: string, options?: PexelsImageOptions) => {
-      let url = `https://api.pexels.com/v1/search?query=${text}`;
+    getImage: async (q: string, options?: PexelsImageOptions) => {
+      let url = `https://api.pexels.com/v1/search?query=${q}`;
       if (options) {
         for (const [key, value] of Object.entries(options)) {
           if (value === undefined) continue;
@@ -17,12 +19,13 @@ const pexelsapis = (apiKey: string) => {
         method: 'GET',
         headers,
       });
+      if (!isJson(res)) throw new Error(`${res.status}: ${res.statusText}`);
       const data: PhotosWithTotalResults = await res.json();
       if (!res.ok) throw new Error('Pexels API error.');
       return data.photos;
     },
-    getVideo: async (text: string, options?: PexelsVideoOptions) => {
-      let url = `https://api.pexels.com/videos/search?query=${text}`;
+    getVideo: async (q: string, options?: PexelsVideoOptions) => {
+      let url = `https://api.pexels.com/videos/search?query=${q}`;
       if (options) {
         for (const [key, value] of Object.entries(options)) {
           if (value === undefined) continue;
@@ -33,6 +36,7 @@ const pexelsapis = (apiKey: string) => {
         method: 'GET',
         headers,
       });
+      if (!isJson(res)) throw new Error(`${res.status}: ${res.statusText}`);
       const data: Videos = await res.json();
       if (!res.ok || !data.videos) throw new Error(`Pexels: ${data.status} ${data.error}`);
       return data.videos;
